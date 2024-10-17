c5fb8653493d2995ec3f9c799ec875b4f8a92d8f
Francisco Da Silva <phrancysko94@gmail.com>
2024-10-16 21:38:40 -0300

    auala07

---
+++ praticas/Pratica06/estrutura_switch.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{ 
+
+  int nota;
+ 
+ printf("Digite a nota (1 a 5): ");
+   scanf("%d", &nota);
+  switch(nota) { 
+}
+
+
+ case 1: printf("Voce ganhou '*'.\n");
+
+case 2: printf("Voce ganhou '**'.\n");
+
+case 3: printf("Voce ganhou '***'.\n");
+
+case 4: printf("Voce ganhou ****!\n");
+
+case 5: printf("Voce ganhou *****!\n");
+
+default: printf("Nota invalida! Tente novamente.\n");
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    return 0;
+}
